e2d75b103f604bc0d09f167cacfa5d87f30d7544
tolakivandi <tolakivandi90@gmail.com>
2024-09-01 22:34:08 +0700

    two

--- resources/views/dashboard/online_course.blade.php
+++ resources/views/dashboard/online_course.blade.php
@@ -1996,26 +1996,26 @@
 
 
     <!-- JS here -->
-    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/jquery.odometer.min.js"></script>
-    <script src="assets/js/jquery.appear.js"></script>
-    <script src="assets/js/tween-max.min.js"></script>
-    <script src="assets/js/select2.min.js"></script>
-    <script src="assets/js/swiper-bundle.min.js"></script>
-    <script src="assets/js/jquery.marquee.min.js"></script>
-    <script src="assets/js/tg-cursor.min.js"></script>
-    <script src="assets/js/vivus.min.js"></script>
-    <script src="assets/js/ajax-form.js"></script>
-    <script src="assets/js/svg-inject.min.js"></script>
-    <script src="assets/js/jquery.circleType.js"></script>
-    <script src="assets/js/jquery.lettering.min.js"></script>
-    <script src="assets/js/plyr.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/aos.js"></script>
-    <script src="assets/js/main.js"></script>
+    <script src="{{ asset('')assets/js/vendor/jquery-3.6.0.min.js }}"></script>
+    <script src="{{ asset('')assets/js/bootstrap.min.js }}"></script>
+    <script src="{{ asset('')assets/js/imagesloaded.pkgd.min.js }}"></script>
+    <script src="{{ asset('')assets/js/jquery.magnific-popup.min.js }}"></script>
+    <script src="{{ asset('')assets/js/jquery.odometer.min.js }}"></script>
+    <script src="{{ asset('')assets/js/jquery.appear.js }}"></script>
+    <script src="{{ asset('')assets/js/tween-max.min.js }}"></script>
+    <script src="{{ asset('')assets/js/select2.min.js }}"></script>
+    <script src="{{ asset('')assets/js/swiper-bundle.min.js }}"></script>
+    <script src="{{ asset('')assets/js/jquery.marquee.min.js }}"></script>
+    <script src="{{ asset('')assets/js/tg-cursor.min.js }}"></script>
+    <script src="{{ asset('')assets/js/vivus.min.js }}"></script>
+    <script src="{{ asset('')assets/js/ajax-form.js }}"></script>
+    <script src="{{ asset('')assets/js/svg-inject.min.js }}"></script>
+    <script src="{{ asset('')assets/js/jquery.circleType.js }}"></script>
+    <script src="{{ asset('')assets/js/jquery.lettering.min.js }}"></script>
+    <script src="{{ asset('')assets/js/plyr.min.js }}"></script>
+    <script src="{{ asset('')assets/js/wow.min.js }}"></script>
+    <script src="{{ asset('')assets/js/aos.js }}"></script>
+    <script src="{{ asset('')assets/js/main.js }}"></script>
     <script>
         SVGInject(document.querySelectorAll("img.injectable"));
     </script>
